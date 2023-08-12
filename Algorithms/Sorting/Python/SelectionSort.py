be279b3e5cfaf57ab67f8d283d9cc27938a204bc
Göktuğ Gürler <93764453+Goktugrlr@users.noreply.github.com>
2023-08-12 17:49:57 +0300

    Update SelectionSort.py

--- Algorithms/Sorting/Python/SelectionSort.py
+++ Algorithms/Sorting/Python/SelectionSort.py
@@ -1,17 +1,28 @@
-def selection_sort(numbers):
-    for i in range(0, len(numbers)):
+def selection_sort(arr):
+    for i in range(0, len(arr)):
         minIndex = i
-        for j in range(i + 1, len(numbers)):
-            if numbers[j] < numbers[minIndex]:
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[minIndex]:
                 minIndex = j
 
-        temp = numbers[minIndex]
-        numbers[minIndex] = numbers[i]
-        numbers[i] = temp
+        temp = arr[minIndex]
+        arr[minIndex] = arr[i]
+        arr[i] = temp
+        #arr[i], arr[minIndex] = arr[minIndex], arr[i]
 
 
-numberList = [8, 20, 3, 43, 12, 125, 86, 63, 28]
+# Sorting integers
+numbers = [8, 20, 3, 43, 12, 125, 86, 63, 28]
+selection_sort(numbers)
+for sortedNumbers in numbers:
+    print(sortedNumbers, end=" ")
 
-selection_sort(numberList)
+print()
+# Sorting strings
+names = ["Oliver", "Nathan", "Ellie", "Jorge", "Eleni"]
+selection_sort(names)
+for sortedNames in names:
+    print(sortedNames, end=" ")
 
-print(numberList)
+# Note: In contrast to languages like Java and C#, Python's dynamic typing and duck typing enable this function to work
+# with various data types, similar to how a generic function behaves in Java or C#.
