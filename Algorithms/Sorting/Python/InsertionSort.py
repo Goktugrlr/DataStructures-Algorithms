4542dcea9aeb5b9665c81599a42b32dc73544b2c
Göktuğ Gürler <93764453+Goktugrlr@users.noreply.github.com>
2023-08-13 22:57:24 +0300

    Update InsertionSort.py

--- Algorithms/Sorting/Python/InsertionSort.py
+++ Algorithms/Sorting/Python/InsertionSort.py
@@ -1,17 +1,27 @@
-def insertion_sort(numbers):
-    for i in range(1, len(numbers)):
-        key = numbers[i]
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
         j = i - 1
 
-        while j >= 0 and numbers[j] > key:
-            numbers[j + 1] = numbers[j]
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
             j -= 1
 
-        numbers[j + 1] = key
+        arr[j + 1] = key
 
 
-numberList = [8, 20, 3, 43, 12, 125, 86, 63, 28]
+# Sorting integers
+numbers = [8, 20, 3, 43, 12, 125, 86, 63, 28]
+insertion_sort(numbers)
+for sortedNumbers in numbers:
+    print(sortedNumbers, end=" ")
 
-insertion_sort(numberList)
+print()
+# Sorting strings
+names = ["Oliver", "Nathan", "Ellie", "Jorge", "Eleni"]
+insertion_sort(names)
+for sortedNames in names:
+    print(sortedNames, end=" ")
 
-print(numberList)
+# Note: In contrast to languages like Java and C#, Python's dynamic typing and duck typing enable this function to work
+# with various data types, similar to how a generic function behaves in Java or C#.
