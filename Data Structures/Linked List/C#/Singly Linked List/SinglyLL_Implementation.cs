6eb5653c5d4e24467a3aa71462a4c04c529e7000
Göktuğ Gürler <93764453+Goktugrlr@users.noreply.github.com>
2023-08-16 23:57:40 +0300

    New methods have been added to SinglyLL_Implementation.cs

--- Data Structures/Linked List/C#/Singly Linked List/SinglyLL_Implementation.cs
+++ Data Structures/Linked List/C#/Singly Linked List/SinglyLL_Implementation.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 
 public class SinglyLinkedList<T>: IEnumerable<T>
 {
-
+    // Represents the first node in the linked list.
     public Node<T> Head { get; private set; }
+
+    // Represents the last node in the linked list.
     public Node<T> Tail { get; private set; }
 
-    public void AddFirst(T data)
+    public void InsertFirst(T data)
     {
         Node<T> node = new(data);
 
@@ -24,7 +26,7 @@ public class SinglyLinkedList<T>: IEnumerable<T>
         }
     }
 
-    public void AddLast(T data)
+    public void InsertEnd(T data)
     {
         Node<T> node = new(data);
 
@@ -39,19 +41,108 @@ public class SinglyLinkedList<T>: IEnumerable<T>
             Tail = node;
         }
     }
-    public void AddLast(IEnumerable<T> data)
+    
+    public void InsertEnd(IEnumerable<T> data)
     {
         foreach (T item in data)
         {
-            AddLast(item);
+            InsertEnd(item);
         }
     }
+
+    public void InsertMiddle(T data)
+    {
+        if (Head is null)
+        {
+            InsertFirst(data);
+            return;
+        }
+
+        int middleIndex = this.Count() / 2;
+        Node<T> current = Head;
+        Node<T> newNode = new Node<T>(data);
+
+        for (int i = 1; i<middleIndex; i++)
+        {
+            current = current.Next;
+        }
+
+        newNode.Next = current.Next;
+        current.Next = newNode;
+        
+        if (newNode.Next == null)
+        {
+            Tail = newNode;
+        }
+    }
+
+    public void InsertAt(int index, T data)
+    {
+        if (index < 0 || index >= this.Count())
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        if (index == 0)
+        {
+            InsertFirst(data);
+            return;
+        }
+
+        Node<T> newNode = new Node<T>(data);
+        Node<T> current = Head;
+
+        for (int i = 1; i < index; i++)
+        {
+            current = current.Next;
+        }
+        
+        newNode.Next = current.Next;
+        current.Next = newNode;
+
+        if (newNode.Next == null)
+        {
+            Tail = newNode;
+        }
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= this.Count())
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        if (index == 0)
+        {
+            RemoveFirst();
+            return;
+        }
+
+        Node<T> current = Head; 
+
+        for (int i = 1; i < index; i++)
+        {
+            current = current.Next;
+        }
+
+        current.Next = current.Next.Next;
+
+        if (current.Next == null)
+        {
+            Tail = current;
+        }
+
+    }
+
     public void RemoveFirst()
     {
         if (Head is not null)
         {
             Head = Head.Next;
         }
+
+        // else list is already empty
     }
 
     public void RemoveLast()
@@ -64,25 +155,22 @@ public class SinglyLinkedList<T>: IEnumerable<T>
 
         if (Head == Tail)
         {
-            // Only one node in the list
+            // Only One Node in the List
             Head = Tail = null;
             return;
         }
 
-        // If we get here, then the list has more than one node
-        var previous = Head;
+        Node<T> previous = Head;
         while (previous.Next != Tail)
         {
             previous = previous.Next;
         }
 
-        // Now, previous points to the node before Tail
         Tail = previous;
         Tail.Next = null;
     }
 
-
-    public void RemoveWithValue(T data)
+    public void RemoveAllByValue(T data)
     {
         if (Head == null)
             return;
@@ -107,13 +195,72 @@ public class SinglyLinkedList<T>: IEnumerable<T>
                 {
                     Tail = previous;
                 }
-
-                return;
             }
-
-            previous = current;
+            else
+            {
+                previous = current;
+            }
+ 
             current = current.Next;
         }
+    }
+
+    public bool Contains(T data)
+    {
+        Node<T> current = Head;
+
+        while(current != null)
+        {
+            if (current.Data.Equals(data))
+            {
+                return true;
+            }
+            current = current.Next;
+        }
+
+        return false;
+    }
+
+    public void Reverse()
+    {
+        Node<T> current = Head;
+        Node<T> next = null;
+        Node<T> prev = null;
+
+        while (current != null)
+        {
+            next = current.Next;
+            current.Next = prev;
+            prev = current;
+            current = next;
+        }
+
+        Tail = Head;
+        Head = prev;
+    }
+
+    public void CountOccurence(T data)
+    {
+        Node<T> current = Head;
+        int counter = 0;
+
+        while(current != null)
+        {
+            if (current.Data.Equals(data))
+            {
+                counter++;
+            }
+
+            current = current.Next;
+        }
+
+        Console.WriteLine(data + " is encountered for " + counter + " times.");
+    }
+
+    public void ClearAll()
+    {
+        Head = null;
+        Tail = null;
     }
 
     public IEnumerator<T> GetEnumerator()
@@ -144,5 +291,4 @@ public class Node<T>
         Data = data;
     }
 
-    public override string ToString() => $"Data: {Data}";
 }
