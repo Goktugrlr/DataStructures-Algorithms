625916d0085fa1b1fe0f1a9c31da67034353560f
Göktuğ Gürler <93764453+Goktugrlr@users.noreply.github.com>
2023-08-20 18:39:14 +0300

    Update Main.java

--- Data Structures/Stack/Java/Main.java
+++ Data Structures/Stack/Java/Main.java
@@ -1,45 +1,56 @@
 public class Main {
 
     public static void main(String[] args) {
-        // Create an Integer stack
-        Stack<Integer> intStack = new Stack<>();
+        try {
+            // Create an Integer Stack
+            Stack<Integer> intStack = new Stack<>();
 
-        intStack.push(10);
-        intStack.push(20);
-        intStack.push(30);
-        intStack.push(40);
+            intStack.push(10);
+            intStack.push(20);
+            intStack.push(30);
+            intStack.push(40);
 
-        System.out.println("int stack contents (LIFO):");
-        intStack.printStack();
+            System.out.println("Integer stack contents (LIFO):");
+            intStack.printStack();
 
-        System.out.println("\nPeek: " + intStack.peek());
-        System.out.println("Popped: " + intStack.pop());
+            System.out.println("\nPeek: " + intStack.peek());
+            System.out.println("Popped: " + intStack.pop());
 
-        System.out.println("\nStack after pop:");
-        intStack.printStack();
+            System.out.println("\nStack after pop:");
+            intStack.printStack();
 
-        System.out.println("\nElement count in the stack: " + intStack.count());
+            System.out.println("\nElement count in the stack: " + intStack.count());
 
-        intStack.push(32);
-        intStack.push(96);
-        intStack.push(64);
-        intStack.push(128);
+            intStack.push(32);
+            intStack.push(96);
+            intStack.push(64);
+            intStack.push(128);
 
-        System.out.println("\nStack after pushes:");
-        intStack.printStack();
+            System.out.println("\nStack after pushes:");
+            intStack.printStack();
 
-        System.out.println();
+            System.out.println();
 
-        // Create a String stack
-        Stack<String> strStack = new Stack<>();
+            // Create a String stack
+            Stack<String> strStack = new Stack<>();
 
-        strStack.push("ABC");
-        strStack.push("DEF");
-        strStack.push("GHI");
+            strStack.push("ABC");
+            strStack.push("DEF");
+            strStack.push("GHI");
 
-        strStack.pop();
+            strStack.pop();
 
-        System.out.println("string stack contents (LIFO):");
-        strStack.printStack();
+            System.out.println("String stack contents (LIFO):");
+            strStack.printStack();
+
+            System.out.println();
+
+            // Create an Integer Stack with Specified Capacity
+            Stack<Integer> intStack2 = new Stack<>(10);
+            intStack2.pop();
+        }
+        catch (IllegalStateException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
